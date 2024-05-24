3e748386b317ad469754f137d185ec50c32ef70b
Felix Zieger <mail@felixzieger.de>
2024-05-25 00:05:39 +0200

    poetry using nix

--- flake.nix
+++ flake.nix
@@ -4,37 +4,31 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in {
+    let
+      pkgs = import nixpkgs { inherit system; };
+      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+    in {
         devShell = pkgs.mkShell {
           name = "docsy dev shell";
           nativeBuildInputs = [
-            (pkgs.python3.withPackages (python-pkgs: [
-              python-pkgs.slack-bolt
-              python-pkgs.openai
-              python-pkgs.gitpython
-              python-pkgs.pygithub
-            ]))
+            pkgs.python3 pkgs.poetry
           ];
           shellHook = ''
             source .env
           '';
         };
 
-        packages.app = pkgs.buildPythonApplication {
-          pname = "docsy";
-          version = "1.0";
-          src = ./.;
+        packages.app = mkPoetryApplication {
+          projectDir = ./.;
         };
 
-        packages.default = pkgs.python3Packages.buildPythonApplication {
-          pname = "docsy";
-          version = "1.0";
-          src = ./.;
+        packages.default = mkPoetryApplication {
+          projectDir = ./.;
         };
     });
 }
