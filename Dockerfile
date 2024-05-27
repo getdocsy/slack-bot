3a9ce036f1f52825bd2870dbea166654c42d7e4a
Felix Zieger <mail@felixzieger.de>
2024-05-27 20:40:52 +0200

    throw out nix and build image using poetry

--- Dockerfile
+++ Dockerfile
@@ -1,29 +1,21 @@
-# Initially created following https://mitchellh.com/writing/nix-with-dockerfiles
-# Nix builder
-FROM nixos/nix:latest AS builder
+FROM python:3.11-buster
 
-# Copy our source and setup our working dir.
-COPY . /tmp/build
-WORKDIR /tmp/build
+RUN pip install poetry==1.4.2
 
-# Build our Nix environment
-RUN nix \
-    --extra-experimental-features "nix-command flakes" \
-    --option filter-syscalls false \
-    build
-
-# Copy the Nix store closure into a directory. The Nix store closure is the
-# entire set of Nix store values that we need for our build.
-RUN mkdir /tmp/nix-store-closure
-RUN cp -R $(nix-store -qR result/) /tmp/nix-store-closure
-
-# Final image is based on scratch. We copy a bunch of Nix dependencies
-# but they're fully self-contained so we don't need Nix anymore.
-FROM scratch
+ENV POETRY_NO_INTERACTION=1 \
+    POETRY_VIRTUALENVS_IN_PROJECT=1 \
+    POETRY_VIRTUALENVS_CREATE=1 \
+    POETRY_CACHE_DIR=/tmp/poetry_cache
 
 WORKDIR /app
 
-# Copy /nix/store
-COPY --from=builder /tmp/nix-store-closure /nix/store
-COPY --from=builder /tmp/build/result /app
-CMD ["/app/bin/app"]
+COPY pyproject.toml poetry.lock ./
+RUN touch README.md
+
+RUN poetry install --without dev --no-root && rm -rf $POETRY_CACHE_DIR
+
+COPY docsy ./docsy
+
+RUN poetry install --without dev
+
+ENTRYPOINT ["poetry", "run", "python", "-m", "docsy"]
---
+++ docsy/__init__.py
@@ -0,0 +1,117 @@
+import os
+import logging
+
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+from docsy.documentation_assistant import DocumentationAssistant
+from docsy.github_manager import GitHubManager
+
+
+logger = logging.getLogger(__name__)
+
+APP_NAME = "Docsy"
+ORGANIZATION_NAME = "Laufvogel Company"
+
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET"),
+)
+gitHubManager = GitHubManager(
+    "felixzieger/congenial-computing-machine",
+    "felixzieger",
+    os.environ.get("GITHUB_TOKEN"),
+)
+ai = DocumentationAssistant()
+
+
+@app.message("thanks")
+def message_learned(message, say):
+    thread_ts = message.get("thread_ts", None) or message["ts"]
+    say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"Hey there <@{message['user']}>! Looks like you learned something there. Should I create a PR against our public docs?",
+                },
+                "accessory": {
+                    "type": "button",
+                    "text": {"type": "plain_text", "text": "Yes, please"},
+                    "action_id": "button_click",
+                },
+            }
+        ],
+        text=f"Hey there <@{message['user']}>! Looks like you learned something there. Should I create a PR against our public docs?",
+        thread_ts=thread_ts,
+    )
+
+
+@app.action("button_click")
+def action_button_click(body, ack, say, client, channel_id):
+    ack()
+    thread_ts = body["container"]["thread_ts"]
+    say(
+        f"All right, <@{body['user']['username']}>. I'll get back to you with a suggestion",
+        thread_ts=thread_ts,
+    )
+
+    thread = client.conversations_replies(channel=channel_id, ts=thread_ts).data[
+        "messages"
+    ]
+    messages = [
+        (message["user"], message["text"])
+        for message in thread
+        if "user" in message and "text" in message
+    ]
+
+    file_paths = gitHubManager.list_md_files()
+    file_path_suggestion = ai.get_file_path_suggestion(messages, file_paths)
+
+    logger.info(f"file_path_suggestion: {file_path_suggestion}")
+
+    file_content = gitHubManager.get_file_content(file_path_suggestion)
+    file_content_suggestion = ai.get_file_content_suggestion(
+        messages, file_path_suggestion, file_content
+    )
+
+    logger.info(file_content_suggestion)
+    branch_name_suggestion = ai.get_branch_name_suggestion(
+        file_content, file_content_suggestion
+    )
+
+    gitHubManager.create_branch(
+        file_content=file_content_suggestion,
+        relative_file_path=file_path_suggestion,
+        branch_name=branch_name_suggestion,
+    )
+    html_url = gitHubManager.create_pr(
+        branch_name_suggestion,
+        branch_name_suggestion,
+        f"I am {APP_NAME}. I am an AI coworker at {ORGANIZATION_NAME}. I creaetd this PR based on a discussion I observed. Please merge or close as you see fit!",
+    )
+
+    url_block = {
+        "type": "section",
+        "text": {
+            "type": "mrkdwn",
+            "text": f"I opened a PR with that change. How does <{html_url}|this> look?",
+        },
+    }
+
+    app.client.chat_postMessage(
+        channel=channel_id, text="Placeholder", blocks=[url_block], thread_ts=thread_ts
+    )
+
+
+@app.event("message")
+def handle_message_events(body, logger):
+    logger.info(body)
+
+
+# Start your app
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    app.start(port=int(os.environ.get("PORT",3000)))
+
