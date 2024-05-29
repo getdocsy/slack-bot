2ea22746d65804f5cccf0ec7fa651ebaeb92378b
Felix Zieger <mail@felixzieger.de>
2024-05-29 22:19:52 +0200

    persist oauth data

--- README.md
+++ README.md
@@ -19,6 +19,11 @@ Start ngrok in a separate pane
 ngrok http --domain reasonably-firm-cricket.ngrok-free.app 3000
 ```
 
+## Deployment using Docker
+
+The app needs a persistent volume that is mapped to `/app/data/` within the container.
+
+
 ## Troubleshooting
 
 Check ngrok enpoints https://dashboard.ngrok.com/cloud-edge/endpoints
--- docsy/main.py
+++ docsy/main.py
@@ -24,9 +24,9 @@ oauth_settings = OAuthSettings(
     client_id=os.environ["SLACK_CLIENT_ID"],
     client_secret=os.environ["SLACK_CLIENT_SECRET"],
     scopes=["channels:read", "chat:write"],
-    installation_store=FileInstallationStore(base_dir="./data/installations"),
+    installation_store=FileInstallationStore(base_dir="./data/slack/installations"),
     state_store=FileOAuthStateStore(
-        expiration_seconds=600, base_dir="/tmp/data/states"
+        expiration_seconds=600, base_dir="./data/slack/states"
     ),
 )
 app = App(signing_secret=SLACK_SIGNING_SECRET, oauth_settings=oauth_settings)
