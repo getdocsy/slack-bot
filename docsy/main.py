27538016fd704026d9a506faede4278cc41753fd
Felix Zieger <mail@felixzieger.de>
2024-06-10 21:06:50 +0200

    fix team id lookup

--- docsy/main.py
+++ docsy/main.py
@@ -78,6 +78,39 @@ def log_request(logger, body, next):
     return next()
 
 
+@app.event("app_mention")
+def app_mention(message, say):
+    thread_ts = message.get("thread_ts", None) or message["ts"]
+    say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"Hey there <@{message['user']}>, thanks for summoning me! Should I create a PR against our public docs?",
+                },
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {"type": "plain_text", "text": "Yes, please"},
+                        "action_id": "button_click_yes",
+                    },
+                    {
+                        "type": "button",
+                        "text": {"type": "plain_text", "text": "No, thanks"},
+                        "action_id": "button_click_no",
+                    },
+                ],
+            },
+        ],
+        text=f"Hey there <@{message['user']}>, thanks for summoning me! Should I create a PR against our public docs?",
+        thread_ts=thread_ts,
+    )
+
+
 @app.message("thanks")
 def message_learned(message, say):
     thread_ts = message.get("thread_ts", None) or message["ts"]
