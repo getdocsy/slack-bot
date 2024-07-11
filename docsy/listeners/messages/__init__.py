47df1fd0bc29be50b624fa0ddab691193ac2a297
Felix Zieger <mail@felixzieger.de>
2024-07-11 22:52:02 +0200

    remove thanks as trigger keyword

--- docsy/listeners/messages/__init__.py
+++ docsy/listeners/messages/__init__.py
@@ -1,7 +1,4 @@
-import re
-
 from slack_bolt import App
-from .sample_message import sample_message_callback
 
 
 def message_learned_callback(message, say):
@@ -37,5 +34,5 @@ def message_learned_callback(message, say):
 
 
 def register(app: App):
-    app.message(re.compile("(hi|hello|hey)"))(sample_message_callback)
-    app.message("thanks")(message_learned_callback)
+    # app.message("thanks")(message_learned_callback)
+    return
