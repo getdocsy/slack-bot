32a745225512dc33fd953a8262c393f8487cd21d
Felix Zieger <mail@felixzieger.de>
2024-07-17 00:49:16 +0200

    move app mention to separate file

--- docsy/listeners/events/__init__.py
+++ docsy/listeners/events/__init__.py
@@ -1,38 +1,6 @@
 from slack_bolt import App
 from .app_home_opened import app_home_opened_callback
-
-
-def app_mention_callback(event, say):
-    user = event["user"]
-    thread_ts = event.get("thread_ts", None) or event["ts"]
-    say(
-        blocks=[
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": f"Hey there <@{user}>, thanks for summoning me! Should I create a PR against our public docs?",
-                },
-            },
-            {
-                "type": "actions",
-                "elements": [
-                    {
-                        "type": "button",
-                        "text": {"type": "plain_text", "text": "Yes, please"},
-                        "action_id": "button_click_yes",
-                    },
-                    {
-                        "type": "button",
-                        "text": {"type": "plain_text", "text": "No, thanks"},
-                        "action_id": "button_click_no",
-                    },
-                ],
-            },
-        ],
-        text=f"Hey there <@{user}>, thanks for summoning me! Should I create a PR against our public docs?",
-        thread_ts=thread_ts,
-    )
+from .app_mention import app_mention_callback
 
 
 def register(app: App):
---
+++ docsy/listeners/events/app_mention.py
@@ -0,0 +1,31 @@
+def app_mention_callback(event, say):
+    user = event["user"]
+    thread_ts = event.get("thread_ts", None) or event["ts"]
+    say(
+        blocks=[
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"Hey there <@{user}>, thanks for summoning me! Should I create a PR against our public docs?",
+                },
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": {"type": "plain_text", "text": "Yes, please"},
+                        "action_id": "button_click_yes",
+                    },
+                    {
+                        "type": "button",
+                        "text": {"type": "plain_text", "text": "No, thanks"},
+                        "action_id": "button_click_no",
+                    },
+                ],
+            },
+        ],
+        text=f"Hey there <@{user}>, thanks for summoning me! Should I create a PR against our public docs?",
+        thread_ts=thread_ts,
+    )
