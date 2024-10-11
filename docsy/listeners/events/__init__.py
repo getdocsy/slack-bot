d4c05bcc931cc9726bd4afed7d154ddbff72da5a
Felix Zieger <fzieger@meshcloud.io>
2024-10-11 20:19:47 +0200

    silence message events in logs

--- docsy/listeners/events/__init__.py
+++ docsy/listeners/events/__init__.py
@@ -2,7 +2,11 @@ from slack_bolt import App
 from .app_home_opened import app_home_opened_callback
 from .app_mention import app_mention_callback
 
+# bolt is spamming the logs with warnings if we don't handle this events
+def handle_message_events():
+    return
 
 def register(app: App):
     app.event("app_mention")(app_mention_callback)
-    app.event("app_home_opened")(app_home_opened_callback)  # Example\n\n
+    app.event("app_home_opened")(app_home_opened_callback)
+    app.event("message")(handle_message_events)
