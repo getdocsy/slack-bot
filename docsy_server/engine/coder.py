d3902029edaea2078e2b717e6267867214fd211d
Felix Zieger <67903933+felixzieger@users.noreply.github.com>
2024-11-04 14:59:30 +0100

    add file pathos to input

--- docsy_server/engine/coder.py
+++ docsy_server/engine/coder.py
@@ -27,7 +27,7 @@ class DocsyCoder:
             "/ask" + "\n"
             "The following changes are made to the code:" + str(github_repo_context) + "\n"
             "This is the current structure of the documentation:" + "\n".join(file_paths) + "\n"
-            "Which files in the documentation needs to be updated to reflect the changes? " + "\n"
+            "Which files in the documentation need to be updated to reflect the changes? " + "\n"
             "Answer with a JSON object with the following format: " + "\n"
             "{\"files\": [{\"path\": \"path/to/file\", \"action\": \"+\", \"explanation\": \"explanation of why this file needs to be updated\"}]}" + "\n"
             "Where 'path' is the path to the file that needs to be updated, and 'action' is '+' if the file should be created, '-' if the file should be deleted, or '~' if the file should be modified." + "\n"
@@ -38,8 +38,10 @@ class DocsyCoder:
     def suggest(self, github_repo_context: GithubRepositoryContext, file_paths: list[str]) -> list[FileSuggestion]:
         prompt = self._get_suggest_prompt(github_repo_context, file_paths)
         logger.info(f"Running with prompt: {prompt}")
+        file_paths = self.github_manager.resolve_file_paths(file_paths)
         coder = AiderCoder.create(main_model=AIDER_MODEL, fnames=file_paths, io=io)
         coder.run(prompt)
+        # TODO parse the output and return the FileSuggestions
         
     def apply(self, suggestion: Suggestion, context: GithubRepositoryContext) -> dict:
         branch_name = f"docsy-suggestion"
